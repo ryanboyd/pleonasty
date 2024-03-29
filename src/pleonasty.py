b8a08044a2d1c87b38e92617bee3a5b2afc4516a
Ryan Boyd <ryan@ryanboyd.io>
2024-03-28 20:32:06 -0400

    v0.0.4
    added a small "quality of life" feature where you can have pleonasty kick out a prompt set as a string in the template associated with your loaded model.

--- src/pleonasty.py
+++ src/pleonasty.py
@@ -309,6 +309,34 @@ class Pleonast():
         self.set_message_context(prompt_messages)
         return
 
+    def convert_prompt_to_template_str(self, prompt_messages: list) -> str:
+        """
+                Converts a prompt set (in the form of a list of dictionaries) to a templated string. Helpful for setting
+                up some data for fine-tuning. Your prompt set should be in a format like this one:
+
+                prompt_messages = [
+                                    {"role": "system", "content": "Please answer the math question."},
+                                    {"role": "user", "content": "1+1=?"},  # example 1
+                                    {"role": "assistant", "content": "2"},  # example 1
+                                    {"role": "user", "content": "1+2=?"},  # example 2
+                                    {"role": "assistant", "content": "3"},  # example 2
+                                    {"role": "user", "content": "2+2=?"}
+                                ]
+
+                :return:
+                """
+
+        if not isinstance(prompt_messages, list):
+            raise MessageContextException("It appears that your prompt set is not a list of dictionaries.")
+
+        for item in prompt_messages:
+            if not isinstance(item, dict):
+                raise MessageContextException("""Your prompt set need to be contained in dictionaries.
+                         Each dictionary must have both a "role" and "content" key.""")
+
+        return self.tokenizer.apply_chat_template(prompt_messages,  tokenize=False)
+
+
 
 # some potential stuff for parsing jsons
 #import ast
