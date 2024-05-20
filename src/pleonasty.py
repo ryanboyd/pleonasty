c79783f61b6c2f6582b567ae82118294bc1cf29a
Ryan Boyd <ryan@ryanboyd.io>
2024-05-20 16:04:42 -0400

    v0.0.7
    added "chat mode"

--- src/pleonasty.py
+++ src/pleonasty.py
@@ -66,6 +66,8 @@ class Pleonast():
                  hf_token: str = None
                  ):
 
+        self.model_name_str = model
+        self.tokenizer_name_str = tokenizer
         self.result = None
         self.message_context = []
         #self.llm_format = llm_format
@@ -339,6 +341,43 @@ class Pleonast():
         return self.tokenizer.apply_chat_template(prompt_messages, 
                                                   tokenize=False,
                                                   add_generation_prompt=True)
+
+    def chat_mode(self, max_length: int = 1000, bot_name: str = "Bot") -> None:
+    
+        # just disabling all warnings for now, as there appear to be many warnings that are not relevant
+        transformers.logging.set_verbosity_error()
+    
+        print("Type 'quit' (without the quotes) to exit chat mode.")
+        text = ""
+        step = 0
+        while True:
+            # take user input
+            text = input(">> You:")
+
+            if text.strip() == "quit":
+                break
+
+            # encode the input and add end of string token
+            input_ids = self.tokenizer.encode(text + self.tokenizer.eos_token, return_tensors="pt")
+            # concatenate new user input with chat history (if there is)
+            bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
+            step += 1
+            # generate a bot response
+            chat_history_ids = self.model.generate(
+                bot_input_ids,
+                max_length=max_length,
+                do_sample=True,
+                top_k=10,
+                temperature=.75,
+                pad_token_id=self.tokenizer.eos_token_id,
+            )
+            #print the output
+            output = self.tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0],
+                                           skip_special_tokens=True)
+            print(f"\r\n{bot_name}: {output.strip('assistant').strip()}\r\n")
+        
+        transformers.logging.set_verbosity_info()
+        return
 
 
 
